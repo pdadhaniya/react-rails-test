2a6fe66a8346070bb27de237c6cb448ce9731501
Parag Dadhaniya <paragdadhaniya@gmail.com>
2016-01-23 14:23:11 -0600

    Add react-rails gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require react
+//= require react_ujs
+//= require components
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
